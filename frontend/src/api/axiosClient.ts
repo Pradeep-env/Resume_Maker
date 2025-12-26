2f95c33483a1cc885de19e357c762728e0e36ed8
Pradeepkh312 <pradeepkh312@gmail.com>
2025-12-26 17:40:10 +0530

    axios client update

--- frontend/src/api/axiosClient.ts
+++ frontend/src/api/axiosClient.ts
@@ -1,8 +1,14 @@
 import axios from "axios";
+
 const apiUrl = import.meta.env.VITE_API_URL;
+
 const axiosClient = axios.create({
   baseURL: apiUrl, 
-  withCredentials: true, 
+  withCredentials: true, // Required to send/receive cookies
+  
+  // Flask-JWT-Extended defaults:
+  xsrfCookieName: "csrf_access_token", // The name of the cookie Flask sends
+  xsrfHeaderName: "X-CSRF-TOKEN",      // The header name Flask expects
 });
 
 export default axiosClient;
